13ec43a5ddf9129301a17c9f288b40d5df54b18d
Scravt <34680110ax@gmail.com>
2025-10-04 23:21:02 -0300

    first commit. cap 1 y 2

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,41 @@
-use std::io; // std library input/output
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new(); //las variables son inmutables agregar mut para hacerla mutable
+    println!("The secret number is: {secret_number}");
 
-    io::stdin()
-        .read_line(&mut guess)  // & indica que es una referencia es inmutable, &mut es muta
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {guess}");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        //rust nos permite sombrear el valor de una variable, así que podemos usar el mismo nombre (se usa mucho apra cambiar tipos)
+        println!("You guessed: {guess}");
+
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal =>{
+                println!("You win!");
+                break;
+            }
+        }
+    }
+
+
 }
